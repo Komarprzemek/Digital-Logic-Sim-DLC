c468d56658dd7a1dbaaa1034473d423a3a63b009
Komarprzemek <97411995+Komarprzemek@users.noreply.github.com>
2025-04-06 16:40:52 +0200

    Adding middle mouse button as option to remove chips

--- Assets/Modules/Chip Creation/Scripts/Chip Editor/ChipDeleter.cs
+++ Assets/Modules/Chip Creation/Scripts/Chip Editor/ChipDeleter.cs
@@ -22,7 +22,7 @@ namespace DLS.ChipCreation
 
 		void HandleInput()
 		{
-			if (Keyboard.current.backspaceKey.wasPressedThisFrame)
+			if ((Keyboard.current.backspaceKey.wasPressedThisFrame) || (Mouse.current.middleButton.wasPressedThisFrame))
 			{
 				DeleteSelectedChips();
 			}
